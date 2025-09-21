19bcc8c6ba9365a825ca8a140c6c3ae865bea65d
Aiden Youn <78311775+aidenyoun@users.noreply.github.com>
2025-09-21 16:48:57 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.05 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        int tmp1 = 1;
+        int tmp2 = 0;
+        
+        for (int i = 0; i < num_list.length; i++) {
+            tmp1 *= num_list[i];
+            tmp2 += num_list[i];
+        }
+        if (tmp1 < (int) Math.pow(tmp2, 2)) answer = 1;
+        else answer = 0;
+        
+        return answer;
+    }
+}
