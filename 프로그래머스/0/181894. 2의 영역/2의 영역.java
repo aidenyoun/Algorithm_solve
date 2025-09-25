ea4fe9d04a32c3cf1f45ed6fe4616f0246f6bc57
Aiden Youn <78311775+aidenyoun@users.noreply.github.com>
2025-09-25 14:43:25 +0900

    [level 0] Title: 2의 영역, Time: 2392.55 ms, Memory: 122 MB -BaekjoonHub

--- 프로그래머스/0/181894. 2의 영역/2의 영역.java
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.java
@@ -1,27 +1,6 @@
-import java.util.*;
-
 class Solution {
     public int[] solution(int[] arr) {
-        List<Integer> al = new ArrayList<>();
-        
-        
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == 2) {
-                al.add(i);
-            }
-        }
-        if (al.size() == 0) {
-            int[] answer = new int[1];
-            answer[0] = -1;
-            return answer;
-        }
-        int[] answer = new int[Collections.max(al) - Collections.min(al) + 1];
-        int cnt = 0;
-        for (int i = Collections.min(al); i < Collections.max(al) + 1; i++) {
-            answer[cnt] = arr[i];
-            cnt ++;
-        }
-        
+        int[] answer = {};
         return answer;
     }
 }
