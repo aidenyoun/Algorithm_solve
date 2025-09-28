03ae1b079a21a570250e10970c2699501a63912b
Aiden Youn <78311775+aidenyoun@users.noreply.github.com>
2025-09-28 11:24:02 +0900

    [level 0] Title: 원하는 문자열 찾기, Time: 7.03 ms, Memory: 85.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(String myString, String pat) {
+        
+        String lowerMyString = myString.toLowerCase();
+        String lowerPat = pat.toLowerCase();
+        
+        if (lowerMyString.contains(lowerPat)) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+}
