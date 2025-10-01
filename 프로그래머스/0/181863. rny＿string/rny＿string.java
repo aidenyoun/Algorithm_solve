a092656e5211d1451aa4111edf3cbc660d9f803b
Aiden Youn <78311775+aidenyoun@users.noreply.github.com>
2025-10-01 16:52:46 +0900

    [level 0] Title: rny_string, Time: 0.07 ms, Memory: 80 MB -BaekjoonHub

---
+++ 프로그래머스/0/181863. rny＿string/rny＿string.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String rny_string) {
+        String answer = "";
+        
+        answer = rny_string.replace("m", "rn");
+
+        
+        return answer;
+    }
+}
