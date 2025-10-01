e7f9a049cb983189a875ccbdc3b6f0063bb7b7d3
Aiden Youn <78311775+aidenyoun@users.noreply.github.com>
2025-10-01 14:31:16 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 3.12 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(String myString, String pat) {
+        int answer = 0;
+        String tmp = "";
+        for (int i = 0; i < myString.length(); i++) {
+            if (myString.charAt(i) == 'A') {
+                tmp += "B";
+            }
+            else tmp += "A";
+        }
+        
+        if (tmp.contains(pat)) {
+            answer = 1;
+        }
+        
+        return answer;
+    }
+}
